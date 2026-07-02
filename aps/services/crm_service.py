774d94502e2ebfe57489f373733b96072b2ceba2
hkms65-rgb <hkms65@gmail.com>
2026-07-02 17:34:26 +0300

    Add optional CRM repository support

--- aps/services/crm_service.py
+++ aps/services/crm_service.py
@@ -1,5 +1,22 @@
 class CRMService:
-    def __init__(self): self.accounts=[]
-    def create_account(self,name,segment='general'):
-        a={'name':name,'segment':segment}; self.accounts.append(a); return a
-    def list_accounts(self): return list(self.accounts)
+    def __init__(self, repository=None):
+        self.repository = repository
+        self.accounts = []
+
+    def create_account(self, name, segment='general'):
+        if not name:
+            raise ValueError('Account name is required')
+        if self.repository is not None:
+            account = self.repository.create_account(name, segment)
+            return {'id': account.id, 'name': account.name, 'segment': account.segment}
+        account = {'name': name, 'segment': segment}
+        self.accounts.append(account)
+        return account
+
+    def list_accounts(self):
+        if self.repository is not None:
+            return [
+                {'id': account.id, 'name': account.name, 'segment': account.segment}
+                for account in self.repository.list_accounts()
+            ]
+        return list(self.accounts)
