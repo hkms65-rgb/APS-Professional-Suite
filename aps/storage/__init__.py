27bb85ba5fe573648639e32e26cd6a20a260d468
hkms65-rgb <hkms65@gmail.com>
2026-07-02 17:39:12 +0300

    Export database migrations

--- aps/storage/__init__.py
+++ aps/storage/__init__.py
@@ -1,4 +1,12 @@
 from .database import Database, DatabaseConfig
+from .migrations import Migration, MigrationRunner
 from .repositories import CRMRepository, FinanceRepository
 
-__all__ = ["Database", "DatabaseConfig", "CRMRepository", "FinanceRepository"]
+__all__ = [
+    "Database",
+    "DatabaseConfig",
+    "Migration",
+    "MigrationRunner",
+    "CRMRepository",
+    "FinanceRepository",
+]
